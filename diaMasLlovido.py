e27c64f9c9c4dcfeef350d95cc6f44fa9eafc8a0
guillecoder <guillermo@toptive.co>
2020-11-10 12:20:42 -0300

    Emprolijando con parametros

--- diaMasLlovido.py
+++ diaMasLlovido.py
@@ -7,13 +7,13 @@ class MyApp(App):
     def OnInit(self):
         f = Frame(None, -1, "Dia mas llovido")
         p = Panel(f)
-        lun      = TextCtrl()
-        mar      = TextCtrl()
-        mier     = TextCtrl()
-        jueves   = TextCtrl()
-        viernes  = TextCtrl()
-        sabado   = TextCtrl()
-        domingo  = TextCtrl()
+        lun      = TextCtrl(p, -1, "Miligramos llovidos el Lunes?",    (0,0),   (250,30))
+        mar      = TextCtrl(p, -1, "Miligramos llovidos el Martes?",   (0,45),  (250,30))
+        mier     = TextCtrl(p, -1, "Miligramos llovidos el Miercoles?",(0,110),  (250,30))
+        jue      = TextCtrl(p, -1, "Miligramos llovidos el Jueves?",   (0,165), (250,30))
+        vier     = TextCtrl(p, -1, "Miligramos llovidos el Viernes?",  (0,220), (250,30))
+        sab      = TextCtrl(p, -1, "Miligramos llovidos el Sabado?",   (0,280), (250,30))
+        dom      = TextCtrl(p, -1, "Miligramos llovidos el Domingo?",  (0,340), (250,30))
         f.Show()
         return True
 
